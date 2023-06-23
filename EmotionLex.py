689470ed600df980d85401b86212f4a87ae48974
BenjaminLopezLagos <benjamin.lopezl@userena.cl>
2023-06-23 00:00:24 -0400

    Added a way to handle negative words and boost scores in certain situations (words containing "very" as prefix).

--- EmotionLex.py
+++ EmotionLex.py
@@ -1,5 +1,6 @@
 import json
 import nltk
+import functools
 import re
 from abc import ABC
 import pandas as pd
@@ -7,6 +8,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import wordnet
 
 
 class EmotionLex:
@@ -25,12 +27,8 @@ class DetectionStrategy(ABC):
 class NRCLex(DetectionStrategy):
     def __init__(self, lex_file='nrc_en.json'):
         self.lexicon = pd.read_csv("nrclex.txt", sep="	", header=None, names=["word", "emotion", "is_present"])
-        # with open(lex_file, 'r') as json_file:
-        #    self.lexicon = json.load(json_file)
 
     def execute(self, phrase):
-        # df = pd.read_csv("nrclex.txt", sep="	", header=None, names=["word", "emotion", "is_present"])
-        unique_elements = self.lexicon["word"].unique()
         print(self.lexicon.shape)
         tokens = phrase.clean_phrase()
         print(tokens)
@@ -38,11 +36,18 @@ class NRCLex(DetectionStrategy):
                     'surprise': 0.0, 'positive': 0.0, 'negative': 0.0, 'sadness': 0.0,
                     'disgust': 0.0, 'joy': 0.0}
         for w in tokens:
+            # in progress, it's supposed to boost words that have "very" as prefix
+            multiplier = 1
+            # If there's a particular word inside the token
+            if w.find("not") != -1:
+                w = change_negative_word_to_antonym(w)
+            if w.find("very") != -1:
+                multiplier = 1.5
+                w = remove_very_from_token(w)
             if self.lexicon["word"].str.contains(w).any():
                 word_lex_df = self.lexicon.query("word == @w & is_present > 0")
-                # print(word_lex_df.head())
                 for index, row in word_lex_df.iterrows():
-                    emotions[row["emotion"]] += 1
+                    emotions[row["emotion"]] += 1 * multiplier
 
         return emotions
 
@@ -58,22 +63,66 @@ class Phrase:
         self.text = text
 
     def decontracted(self):
-        phrase = re.sub(r"n\'t", " "+"not", self.text)
-        phrase = re.sub(r"\'re", " "+"are", phrase)
-        phrase = re.sub(r"\'s", " "+"is", phrase)
-        phrase = re.sub(r"\'d", " "+"would", phrase)
-        phrase = re.sub(r"\'ll", " "+"will", phrase)
-        phrase = re.sub(r"\'ve", " "+"have", phrase)
-        phrase = re.sub(r"\'m", " "+"am", phrase)
+        phrase = re.sub(r"n\'t", " " + "not", self.text)
+        phrase = re.sub(r"\'re", " " + "are", phrase)
+        phrase = re.sub(r"\'s", " " + "is", phrase)
+        phrase = re.sub(r"\'d", " " + "would", phrase)
+        phrase = re.sub(r"\'ll", " " + "will", phrase)
+        phrase = re.sub(r"\'ve", " " + "have", phrase)
+        phrase = re.sub(r"\'m", " " + "am", phrase)
         return phrase
 
     def clean_phrase(self):
         modified_phrase = self.decontracted()
         stop_words = set(stopwords.words('english'))
         stop_words.remove('not')
+        stop_words.remove('very')
         word_tokens = word_tokenize(modified_phrase)
         filtered_sentence = [w.lower() for w in word_tokens if w not in stop_words and w.isalnum()]
         filtered_sentence = [WordNetLemmatizer().lemmatize(w) for w in filtered_sentence]
+        filtered_sentence = join_negative_words(filtered_sentence)
+        for i in range(len(filtered_sentence)):
+            if filtered_sentence[i] == "hated":
+                # special case where the word "hated" is not correctly lemmatized
+                filtered_sentence[i] = "hate"
+            if filtered_sentence[i].endswith("ed"):
+                filtered_sentence[i] = WordNetLemmatizer().lemmatize(filtered_sentence[i], pos="v")
 
-        #el pepe
         return filtered_sentence
+
+
+def join_negative_words(token_list: list):
+    index_limit = range(len(token_list) - 1)
+    try:
+        for i in index_limit:
+            if token_list[i] == 'not' or token_list[i] == 'very':
+                token_list[i: i + 2] = [functools.reduce(lambda x, y: x + " " + y, token_list[i: i + 2])]
+    except IndexError:
+        # Might add a fix later but it works for now.
+        print("Stopping token joining process.")
+    return token_list
+
+
+# must contain "not" before a word
+def change_negative_word_to_antonym(word: str):
+    try:
+        new_word = word.replace("not", "").strip()
+        antonyms = get_antonyms(new_word)
+        new_word = antonyms[0]
+        return new_word
+    except IndexError:
+        return word
+
+
+def remove_very_from_token(word: str):
+    new_word = word.replace("very", "").strip()
+    return new_word
+
+
+def get_antonyms(word: str):
+    antonyms = []
+    for syn in wordnet.synsets(word):
+        for lemma in syn.lemmas():
+            if lemma.antonyms():
+                antonyms.extend([antonym.name() for antonym in lemma.antonyms()])
+    return antonyms
